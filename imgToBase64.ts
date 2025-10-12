1c8c7e84f26f9f58d5c215eaf58be4489e2d132c
tygrysek90 <risa.szlachta@gmail.com>
2025-10-12 07:36:59 +0200

    Fix graphics encoding script counting fault, add vscode commands

--- imgToBase64.ts
+++ imgToBase64.ts
@@ -7,6 +7,7 @@ const outFile = "./src/graphics/pluginGraphics.ts"
 
 let files = fs.readdirSync(inDir)
 let output: string
+let filesDone = 0
 
 output = `import { createImageFromBase64 } from "./imageFromBase64"\n`
 output += `export const pluginGraphics = {\n`
@@ -15,6 +16,7 @@ files.forEach(file => {
     if (file.length > 4 && file.slice(-3) == "png") {
         output += `\t${file.slice(0,-4)}: createImageFromBase64("${(fs.readFileSync(`${inDir}/${file}`).toString("base64"))}"),\n`
         console.log(chalk.cyanBright("Processed " + chalk.bold(`${inDir}${file}`)))
+        filesDone++
     }
     else {
         console.log(chalk.magentaBright(`Skipping ${inDir}${file}`))
@@ -24,4 +26,4 @@ output += `}\n`
 
 fs.writeFileSync(outFile, output)
 
-console.log(chalk.bold(chalk.greenBright(`Graphics to base64 done: ${files.length} files packed into "${outFile}"\n`)))
+console.log(chalk.bold(chalk.greenBright(`Graphics to base64 done: ${filesDone} files packed into "${outFile}"\n`)))
